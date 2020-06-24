5b8a7735174790428dc3f78008ac48eeee9b23b0
bunxumal_JHB <66954951+bunxumal@users.noreply.github.com>
2020-06-24 15:37:57 +0200

    Add files via upload

---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bunxumal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 12:17:19 by bunxumal          #+#    #+#             */
+/*   Updated: 2020/06/23 12:26:02 by bunxumal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void ft_ultimatte_ft(int *********nbr)
+{
+	int num;
+	int *ptr;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********ptr9;
+
+	num = 42;
+	ptr = &num;
+	ptr2 = &ptr;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+}
